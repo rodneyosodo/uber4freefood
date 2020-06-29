93641feb8d2ce13152adc02fc6c77dd0b1812135
sammyoina <sammyoina@gmail.com>
2020-06-29 20:30:33 +0300

    enabling patch for all fields

--- Src/Backend/foodApi/models/food.go
+++ Src/Backend/foodApi/models/food.go
@@ -3,37 +3,48 @@ package models
 import "time"
 
 type Food struct {
-	ID					uint	`json:"id" gorm:"primary_key"`
-	FoodName			string	`json:"foodName"`
-	DietType			string	`json:"dietType"`
-	Description			string	`json:"description"`
-	SpecialIngridients	string	`json:"specialIngridients"`
-	Serving				string	`json:"serving"`
-	SpecialNote			string	`json:specialNote"`
-	FoodImageId			string	`json:"foodImageId"`
-	LocationLat			string	`json:"locationLat"`
-	LocationLong		string	`json:"locationLong"`
-	AngelUserID			string	`json:"angelUserID"`
-	HumanUserID			string	`json:"humanUserID"`
-	Status				string	`json:"status"`
-	CreatedAt			time.Time
-	UpdatedAt			time.Time
-	DeletedAt			*time.Time
+	ID                 uint   `json:"id" gorm:"primary_key"`
+	FoodName           string `json:"foodName"`
+	DietType           string `json:"dietType"`
+	Description        string `json:"description"`
+	SpecialIngridients string `json:"specialIngridients"`
+	Serving            string `json:"serving"`
+	SpecialNote        string `json:"specialNote"`
+	FoodImageId        string `json:"foodImageId"`
+	LocationLat        string `json:"locationLat"`
+	LocationLong       string `json:"locationLong"`
+	AngelUserID        string `json:"angelUserID"`
+	HumanUserID        string `json:"humanUserID"`
+	Status             string `json:"status"`
+	CreatedAt          time.Time
+	UpdatedAt          time.Time
+	DeletedAt          *time.Time
 }
 type CreateFoodInput struct {
-	FoodName			string	`json:"foodName" binding:"required"`
-	DietType			string	`json:"dietType"`
-	Description			string	`json:"description" binding:"required"`
-	SpecialIngridients	string	`json:"specialIngridients"`
-	Serving				string	`json:"serving"`
-	SpecialNote			string	`json:specialNote"`
-	FoodImageId			string	`json:"foodImageId"`
-	LocationLat			string	`json:"locationLat" binding:"required"`
-	LocationLong		string	`json:"locationLong" binding:"required"`
-	AngelUserID			string	`json:"angelUserID"`
-	HumanUserID			string	`json:"humanUserID"`
-	Status				string	`json:"status"`
+	FoodName           string `json:"foodName" binding:"required"`
+	DietType           string `json:"dietType"`
+	Description        string `json:"description" binding:"required"`
+	SpecialIngridients string `json:"specialIngridients"`
+	Serving            string `json:"serving"`
+	SpecialNote        string `json:"specialNote"`
+	FoodImageId        string `json:"foodImageId"`
+	LocationLat        string `json:"locationLat" binding:"required"`
+	LocationLong       string `json:"locationLong" binding:"required"`
+	AngelUserID        string `json:"angelUserID"`
+	HumanUserID        string `json:"humanUserID"`
+	Status             string `json:"status"`
 }
 type UpdateStatusInput struct {
-	Status				string	`json:"status"`
+	FoodName           string `json:"foodName"`
+	DietType           string `json:"dietType"`
+	Description        string `json:"description"`
+	SpecialIngridients string `json:"specialIngridients"`
+	Serving            string `json:"serving"`
+	SpecialNote        string `json:"specialNote"`
+	FoodImageId        string `json:"foodImageId"`
+	LocationLat        string `json:"locationLat"`
+	LocationLong       string `json:"locationLong"`
+	AngelUserID        string `json:"angelUserID"`
+	HumanUserID        string `json:"humanUserID"`
+	Status             string `json:"status"`
 }
